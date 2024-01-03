939d84084399f39d93254d1552aababf1d101eb3
Davidcastel26 <davcastellanoslarios@gmail.com>
2024-01-02 18:10:48 -0600

    Initial Commit

---
+++ SwiftUI-weatherApp/SwiftUI_weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_weatherAppApp.swift
+//  SwiftUI-weatherApp
+//
+//  Created by David on 2/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
